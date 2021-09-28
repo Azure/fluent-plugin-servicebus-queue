4bb9c788c8671b88a395d6514e982d509c891266
Tianyu97 <41757571+Tianyu97@users.noreply.github.com>
2021-09-28 10:07:53 -0700

    add msi support for fluent-plugin-servicebus-queue (#5)
    for now, this plugin only supports accesskey. This pr will add msi support so that the user can use msi to get access to service bus queue
    * add msi support
    
    * resolve comments
    
    Co-authored-by: tianyuzhan <tianyuzhan@microsoft.com>

--- lib/fluent/plugin/out_azure_servicebus_queue.rb
+++ lib/fluent/plugin/out_azure_servicebus_queue.rb
@@ -3,6 +3,7 @@ require 'cgi'
 require 'openssl'
 require 'base64'
 require 'net/http'
+require 'faraday'
 
 module Fluent::Plugin
   class AzureServicebusQueue < Output
@@ -10,10 +11,15 @@ module Fluent::Plugin
 
     config_param :namespace, :string
     config_param :queueName, :string
-    config_param :accessKeyName, :string
-    config_param :accessKeyValueFile, :string
+    config_param :accessKeyName, :string, :default => nil
+    config_param :accessKeyValueFile, :string, :default => nil
     config_param :timeToLive, :integer
     config_param :field, :string, :default => "message"
+    config_param :useMSI, :bool, default: false
+    config_param :clientIDFile, :string, :default => nil
+   
+    IMDS_TOKEN_ACQUIRE_URL = "http://169.254.169.254/metadata/identity/oauth2/token" # The managed identities for Azure resources endpoint for the Instance Metadata Service.
+    API_VERSION = "2018-02-01" # the API version for the IMDS endpoint. Please use API version 2018-02-01 or greater.
 
     # method for sync buffered output mode
     def write(chunk)
@@ -21,8 +27,14 @@ module Fluent::Plugin
       split = read.split("\n")
 
       url = "https://#{namespace}.servicebus.windows.net/#{queueName}/messages"
-      keyValue = getAccessKeyValue
-      token = generateToken(url, accessKeyName, keyValue)
+
+      if useMSI
+        client_id = getClientID
+        token = generateMSIToken(client_id)
+      else 
+        keyValue = getAccessKeyValue
+        token = generateToken(url, accessKeyName, keyValue)
+      end
 
       uri = URI.parse(url)
       https = Net::HTTP.new(uri.host, uri.port)
@@ -43,6 +55,10 @@ module Fluent::Plugin
       File.read(accessKeyValueFile).strip
     end
 
+    def getClientID
+      File.read(clientIDFile).strip
+    end
+
     def generateToken(url,key_name,key_value)
       target_uri = CGI.escape(url.downcase).gsub('+', '%20').downcase
       expires = Time.now.to_i + 10
@@ -57,6 +73,22 @@ module Fluent::Plugin
         ).gsub('+', '%20')
 
       "SharedAccessSignature sr=#{target_uri}&sig=#{signature}&se=#{expires}&skn=#{key_name}"
+    end
+
+    # reference1: https://docs.microsoft.com/en-us/rest/api/servicebus/send-message-to-queue (Instruction to send message to a service bus queue using Azure AD JWT token)
+    # reference2: https://docs.microsoft.com/en-us/azure/active-directory/managed-identities-azure-resources/how-to-use-vm-token (Instruction to use managed identities for\ 
+    # Azure resources on an Azure VM to acquire an access token)
+    def generateMSIToken(clientid)
+      access_key_request = Faraday.new(IMDS_TOKEN_ACQUIRE_URL \
+                                      "?api-version=#{API_VERSION}" \
+                                      '&resource=https://servicebus.azure.net/' \
+                                      "&client_id=#{clientid}",
+                                      headers: { 'Metadata' => 'true' })
+                                  .get
+                                  .body
+      access_token = JSON.parse(access_key_request)['access_token']
+
+      "Bearer #{access_token}"
     end
   end
 end
